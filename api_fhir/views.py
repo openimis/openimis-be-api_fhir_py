fcd98fe844beb20a57fd5bbf8b6ccf1e63b67642
Arkadiusz Lalo <alalo@soldevelo.com>
2019-07-05 13:18:34 +0200

    ONCO-56: Added API FHIR permissions validation

--- api_fhir/views.py
+++ api_fhir/views.py
@@ -6,41 +6,42 @@ from rest_framework import viewsets, mixins
 from rest_framework.viewsets import GenericViewSet
 
 from api_fhir.paginations import FhirBundleResultsSetPagination
+from api_fhir.permissions import FHIRApiPermissions
 from api_fhir.serializers import PatientSerializer, LocationSerializer, PractitionerRoleSerializer, \
     PractitionerSerializer, ClaimSerializer
 
 
-class InsureeViewSet(viewsets.ModelViewSet):
+class BaseFHIRView(object):
+    pagination_class = FhirBundleResultsSetPagination
+    permission_classes = (FHIRApiPermissions,)
+
+
+class InsureeViewSet(BaseFHIRView, viewsets.ModelViewSet):
     queryset = Insuree.objects.all()
     serializer_class = PatientSerializer
-    pagination_class = FhirBundleResultsSetPagination
 
 
-class HFViewSet(viewsets.ModelViewSet):
+class HFViewSet(BaseFHIRView, viewsets.ModelViewSet):
     queryset = HealthFacility.objects.all()
     serializer_class = LocationSerializer
-    pagination_class = FhirBundleResultsSetPagination
 
 
-class PractitionerRoleViewSet(viewsets.ModelViewSet):
+class PractitionerRoleViewSet(BaseFHIRView, viewsets.ModelViewSet):
     queryset = ClaimAdmin.objects.all()
     serializer_class = PractitionerRoleSerializer
-    pagination_class = FhirBundleResultsSetPagination
 
     def perform_destroy(self, instance):
         instance.health_facility_id = None
         instance.save()
 
 
-class PractitionerViewSet(viewsets.ModelViewSet):
+class PractitionerViewSet(BaseFHIRView, viewsets.ModelViewSet):
     queryset = ClaimAdmin.objects.all()
     serializer_class = PractitionerSerializer
-    pagination_class = FhirBundleResultsSetPagination
 
 
-class ClaimViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin,
+class ClaimViewSet(BaseFHIRView, mixins.RetrieveModelMixin, mixins.ListModelMixin,
                    mixins.CreateModelMixin, GenericViewSet):
     queryset = Claim.objects.all()
     serializer_class = ClaimSerializer
     lookup_field = 'code'
-    pagination_class = FhirBundleResultsSetPagination
