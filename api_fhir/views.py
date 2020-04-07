7f198ae779ae3afe7606952a3bf5af0422bd67bc
Eric Darchis <darchis@pobox.com>
2020-04-07 18:58:44 +0200

    Switch FHIR security to the OpenIMIS model ⚠️
    So far, the FHIR API was allowed or denied based on Django permissions per module, model and method. This didn't filter out the data that was out of the user's range.
    The authorisations are now taken from the same source as the GraphQL API. It means that it might not be a 100% match.
    
    There might still be a need to adjust some permission mappings or some restrictions.
    
    The automated tests were not yet updated for this change, it was tested manually.

--- api_fhir/views.py
+++ api_fhir/views.py
@@ -1,5 +1,13 @@
 from api_fhir.converters import OperationOutcomeConverter
+from api_fhir.paginations import FhirBundleResultsSetPagination
+from api_fhir.permissions import FHIRApiPermissions, FHIRApiClaimPermissions, FHIRApiEligibilityRequestPermissions, \
+    FHIRApiCoverageRequestPermissions, FHIRApiCommunicationRequestPermissions, FHIRApiPractitionerPermissions, \
+    FHIRApiHFPermissions, FHIRApiInsureePermissions
+from api_fhir.serializers import PatientSerializer, LocationSerializer, PractitionerRoleSerializer, \
+    PractitionerSerializer, ClaimSerializer, ClaimResponseSerializer, CommunicationRequestSerializer
+from claim import ClaimConfig
 from claim.models import ClaimAdmin, Claim, Feedback
+from django.contrib.auth.models import PermissionsMixin
 from django.db.models import OuterRef, Exists
 from insuree.models import Insuree
 from location.models import HealthFacility
@@ -22,7 +30,6 @@ from api_fhir.serializers.coverageSerializer import CoverageSerializer
 
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
-
     def enforce_csrf(self, request):
         return
 
@@ -35,14 +42,14 @@ class BaseFHIRView(APIView):
 
 class InsureeViewSet(BaseFHIRView, viewsets.ModelViewSet):
     lookup_field = 'uuid'
-    queryset = Insuree.objects.all()
     serializer_class = PatientSerializer
+    permission_classes = (FHIRApiInsureePermissions,)
 
     def list(self, request, *args, **kwargs):
         claim_date = request.GET.get('claimDateFrom')
         identifier = request.GET.get("identifier")
 
-        queryset = Insuree.objects.all()
+        queryset = self.get_queryset()
 
         if claim_date is not None:
             day, month, year = claim_date.split('-')
@@ -63,72 +70,99 @@ class InsureeViewSet(BaseFHIRView, viewsets.ModelViewSet):
         serializer = PatientSerializer(self.paginate_queryset(queryset), many=True)
         return self.get_paginated_response(serializer.data)
 
+    def get_queryset(self):
+        return Insuree.get_queryset(None, self.request.user)
 
 class HFViewSet(BaseFHIRView, viewsets.ModelViewSet):
     lookup_field = 'uuid'
-    queryset = HealthFacility.objects.all()
     serializer_class = LocationSerializer
+    permission_classes = (FHIRApiHFPermissions,)
 
     def list(self, request, *args, **kwargs):
         identifier = request.GET.get("identifier")
-        queryset = HealthFacility.objects.filter(validity_to__isnull=True)
+        queryset = self.get_queryset()
         if identifier:
             queryset = queryset.filter(code=identifier)
 
         serializer = LocationSerializer(self.paginate_queryset(queryset), many=True)
         return self.get_paginated_response(serializer.data)
 
+    def get_queryset(self):
+        return HealthFacility.get_queryset(None, self.request.user)
+
 
 class PractitionerRoleViewSet(BaseFHIRView, viewsets.ModelViewSet):
     lookup_field = 'uuid'
-    queryset = ClaimAdmin.objects.all()
     serializer_class = PractitionerRoleSerializer
+    permission_classes = (FHIRApiPractitionerPermissions,)
 
     def perform_destroy(self, instance):
         instance.health_facility_id = None
         instance.save()
 
+    def get_queryset(self):
+        return ClaimAdmin.get_queryset(None, self.request.user)
+
 
 class PractitionerViewSet(BaseFHIRView, viewsets.ModelViewSet):
     lookup_field = 'uuid'
-    queryset = ClaimAdmin.objects.all()
     serializer_class = PractitionerSerializer
+    permission_classes = (FHIRApiPractitionerPermissions,)
 
     def list(self, request, *args, **kwargs):
         identifier = request.GET.get("identifier")
-        queryset = ClaimAdmin.objects.filter(validity_to__isnull=True)
+        queryset = self.get_queryset()
         if identifier:
             queryset = queryset.filter(code=identifier)
 
         serializer = PractitionerSerializer(self.paginate_queryset(queryset), many=True)
         return self.get_paginated_response(serializer.data)
 
+    def get_queryset(self):
+        return ClaimAdmin.get_queryset(None, self.request.user)
+
 
 class ClaimViewSet(BaseFHIRView, mixins.RetrieveModelMixin, mixins.ListModelMixin,
                    mixins.CreateModelMixin, GenericViewSet):
     lookup_field = 'uuid'
-    queryset = Claim.objects.all()
     serializer_class = ClaimSerializer
+    permission_classes = (FHIRApiClaimPermissions,)
+
+    def get_queryset(self):
+        return Claim.get_queryset(None, self.request.user)
 
 
 class ClaimResponseViewSet(BaseFHIRView, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
     lookup_field = 'uuid'
-    queryset = Claim.objects.all()
     serializer_class = ClaimResponseSerializer
+    permission_classes = (FHIRApiClaimPermissions,)
+
+    def get_queryset(self):
+        return Claim.get_queryset(None, self.request.user)
 
 
 class CommunicationRequestViewSet(BaseFHIRView, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
     lookup_field = 'uuid'
-    queryset = Feedback.objects.all()
     serializer_class = CommunicationRequestSerializer
+    permission_classes = (FHIRApiCommunicationRequestPermissions,)
+
+    def get_queryset(self):
+        return Feedback.get_queryset(None, self.request.user)
 
 
 class EligibilityRequestViewSet(BaseFHIRView, mixins.CreateModelMixin, GenericViewSet):
-    queryset = Insuree.objects.none()
+    queryset = Insuree.filter_queryset()
     serializer_class = eval(Config.get_serializer())
+    permission_classes = (FHIRApiEligibilityRequestPermissions,)
+
+    def get_queryset(self):
+        return Insuree.get_queryset(None, self.request.user)
 
 
 class CoverageRequestQuerySet(BaseFHIRView, mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
     lookup_field = 'uuid'
-    queryset = Policy.objects.all()
     serializer_class = CoverageSerializer
+    permission_classes = (FHIRApiCoverageRequestPermissions,)
+
+    def get_queryset(self):
+        return Policy.get_queryset(None, self.request.user)
